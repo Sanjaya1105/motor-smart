e30f21cda49008f5caa6191b01892f8f06110e00
Sanjaya Jayasinghe <njaya100@gmail.com>
2026-06-14 00:12:48 +0530

    add changes final

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/login', function () {
